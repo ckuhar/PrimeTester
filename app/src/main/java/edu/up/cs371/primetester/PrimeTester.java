10339242f49629067c79331abbf34e9ffddb9d10
ckuhar <chloekuhar@gmail.com>
2016-08-31 17:22:29 -0700

    fixed to include 0 and 1 as nonPrime

--- app/src/main/java/edu/up/cs371/primetester/PrimeTester.java
+++ app/src/main/java/edu/up/cs371/primetester/PrimeTester.java
@@ -15,6 +15,11 @@ public class PrimeTester {
      */
     public static boolean isPrime(long n) {
         // for now, return a random result
+        if( n == 0 || n == 1 )
+        {
+            return false;
+        }
+
         for(long i = 2; i < n; i++)
         {
             if(n % i == 0)
